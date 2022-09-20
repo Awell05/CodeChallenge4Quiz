30801b891072750b1ad84732b2c07c9ddb6ce36c
Novo Persona <adcaldwell93@yahoo.com>
2022-09-19 21:23:19 -0400

    added answer selection style to javascript

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var answerSelections = document.querySelectorAll("ol");
+for (var i = 0; i < answerSelections.length; i++){
+    answerSelections[i].setAttribute("style", "width: 15%; background-color: purple; color: white; margin: 0px;");
+}

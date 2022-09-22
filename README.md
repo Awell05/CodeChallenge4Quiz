fecce70c2fc75389f87f7ea0d8aad36ab11d41c2
Novo Persona <adcaldwell93@yahoo.com>
2022-09-21 21:30:08 -0400

    timer function for quiz

--- README.md
+++ README.md
@@ -6,4 +6,15 @@ another question is presneted
 if the answer is incorrect time is substracted from the clock
 if the timer reaches 0 or all questions are answered
 the game is over
-initils and scare are saved
+initils and scare are saved to local storage
+
+
+answer choices should be button with on click 
+after answer choice is clicked= correct or wrong should display and timer should decrease by 10 seconds
+when the timer reaches 0 or all the questions are answered. the game is over
+initials and score will display and should be stored
+if the user would like to review highscores, they should appear once "view Highscores is selected/clicked"
+
+
+
+refer to module 5 activity 7 to change the list items 
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,24 @@
-var answerSelections = document.querySelectorAll("ol");
-for (var i = 0; i < answerSelections.length; i++){
-    answerSelections[i].setAttribute("style", "width: 15%; background-color: purple; color: white; margin: 0px;");
-}
+var startQuizButton = document.querySelector("#startButton");
+var timerPTag = document.querySelector("#timer");
+var timerToCompleteQuiz = 90;
+
+console.log(startQuizButton);
+console.log(timerPTag);
+console.log(timerToCompleteQuiz);
+
+// Function to start timer when "start quiz" button is selected
+// need to add when incorrect answer is selected, 10-15 seconds are deducted from current time
+ startQuizButton.addEventListener('click', function countdown() {
+    console.log('timer should begin');  
+    
+    var timerfunction = setInterval(function(){
+        timerToCompleteQuiz--;
+        timerPTag.textContent = " Timer " + timerToCompleteQuiz;
+        if (timerToCompleteQuiz === 0) {
+            clearInterval(timerfunction);
+        }
+   }, 1000);
+});
+
+
+

30e19e53f86115297cf628dc73d89e459f35f0fb
Novo Persona <adcaldwell93@yahoo.com>
2022-09-24 22:09:38 -0400

    updated function for correct and incorrect answer to display

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,7 @@ var timerPTag = document.querySelector("#timer");
 var timerToCompleteQuiz = 90;
 var currentQuestionIndex = 0
 
+
 var questionsArray = [
     {
         question: "Commonly used data types DO NOT include:",
@@ -82,146 +83,44 @@ function checkIfCorrect(event){
     event.preventDefault()
     console.log("triggered")
     console.log(event.target.innerHTML)
-    if(!questionsArray[currentQuestionIndex.correct]){
+    if(event.target.innerText != questionsArray[currentQuestionIndex].correct){
         console.log("YOU GOT THE BUTTONS TO WORK")
+        incorrectAnswerText = document.createElement("p")
+        quizElement.appendChild(incorrectAnswerText)
+        incorrectAnswerText.innerHTML = "";
+        incorrectAnswerText.textContent = "Wrong Answer";
         timerToCompleteQuiz -= 10
+    }
+    else if (event.target.innerText == questionsArray[currentQuestionIndex].correct){
+        correctAnswerText = document.createElement("p")
+        quizElement.appendChild(correctAnswerText)
+        correctAnswerText.innerHTML = "";
+        correctAnswerText.textContent = "Correct Answer";
+        console.log("You Got the correct Answer")
+    }
+    checkIfQuizOver()
     }
 
 
-    var isDone = checkifQuizOver()
+    var isDone = checkIfQuizOver()
     // you will need to check if you're done with the quiz
     if (!isDone){
-        currentQuestionIndex ++
+       
         displayQuestion()
     } else{
         endQuiz()
     }
     // if you're not done, then move to the next question
-}
 
-function checkifQuizOver(){
-
+function checkIfQuizOver(event){
 console.log("checking if over")
+
+}
     //if it's over return true
     // return false
-}
 
 function endQuiz(){
     console.log("quiz ended condition met.")
+    
     //deduct 10 seconds
 }
-                    
-                    // if(event.target.innerHTML === questionsArray[0].answers[2]){
-                    //     console.log("correct answer");
-                    //     var answersElement = document.getElementById("answer-holder")
-                    //     var correctAnswer = document.createTextNode(" Correct Answer! ")
-                    //     answersElement.appendChild(correctAnswer);}
-                    //     else if (event.target.innerHTML != questionsArray[0].answers[2]){
-                    //         console.log("wrong answer")
-                    //             timerToCompleteQuiz -= 10;
-                    //             var answersElement = document.getElementById("answer-holder")
-                    //             var wrongAnswer = document.createTextNode("Wrong Answer")
-                    //             answersElement.appendChild(wrongAnswer);}}
-                    //             displayQuestion()
-
-
-
-
-
-
-
-
-
-// Displays Question 1
-// function displayQuestion(){
-//     console.log("displayingQuestion1")
-//     var questionElement = document.getElementById("question-holder")
-//     var answersElement = document.querySelector(".ans1")
-    
-//     questionElement.innerHTML= ""
-//     answersElement.innerHTML = ""
-//     questionElement.innerHTML = questionsArray[currentQuestionIndex].question;
-//     for(let i=0; i< questionsArray[currentQuestionIndex].answers.length;i++){
-//         console.log("in the loop")
-//         buttons[i].innerHTML = questionsArray[currentQuestionIndex].answers[i]
-//         buttons.addEventListener("click", checkIfCorrect)
-//     }
-// }
-// Checks if Answer 1 is Correct
-// function checkIfCorrect(event){
-//     event.preventDefault()
-//     console.log("triggered")
-//     console.log(event.target.innerHTML)
-//     if(event.target.innerHTML === questionsArray[0].answers[2]){
-//         console.log("correct answer");
-//         var answersElement = document.getElementById("answer-holder")
-//         var correctAnswer = document.createTextNode(" Correct Answer! ")
-//         answersElement.appendChild(correctAnswer);}
-//         else if (event.target.innerHTML != questionsArray[0].answers[2]){
-//             console.log("wrong answer")
-//                 timerToCompleteQuiz -= 10;
-//                 var answersElement = document.getElementById("answer-holder")
-//                 var wrongAnswer = document.createTextNode("Wrong Answer")
-//                 answersElement.appendChild(wrongAnswer);}}
-                // displayQuestion()
-    
-    // need to increase increment based on user get correct-increament global index
-
-    
-    
-    //     }
-    // //     // Displays Question 2
-    //     function displayQuestion2(){
-    //         console.log("displaying question 2")
-    //         quizElement.style.display = "none"
-    //         question2Element.classList.remove("hidden")
-    //        var answer2Element = document.getElementById("answer2-holder")
-    //         console.log(answer2Element)
-           
-        
-
-
-
-
-
-
-        //     var question2Element = document.getElementById("quiz2-holder")
-        //     var answers2Element = document.querySelector("answer2-holder")
-        //     question2Element.innerHTML= ""
-        //     answers2Element.innerHTML = ""
-        //     question2Element.innerHTML = questionsArray[1].question;
-        // for(let i=0; i< questionsArray[1].answers.length;i++ ){
-        // console.log("in the loop")}
-        // var button = document.createElement("button")
-        // button.innerHTML = questionsArray[currentQuestionIndex].answers[i]
-        // answers2Element.appendChild(button)
-        // button.addEventListener("click", checkIfQ2Correct);
-        // }
-
-    //     function checkIfQ2Correct(event){
-    //         console.log("triggered from q2 ")
-    //        console.log(event.trigger.innerHTML);
-    //     }
-    // }
-//     var isDone = checkifQuizOver()
-//     // you will need to check if you're done with the quiz
-//     if (!isDone){
-//         currentQuestionIndex ++
-//         displayQuestion()
-//     } else{
-//         endQuiz()
-//     }
-//     // if you're not done, then move to the next question
-// }
-
-// function checkifQuizOver(){
-
-// console.log("checking if over")
-//     //if it's over return true
-//     // return false
-// }
-
-// function endQuiz(){
-//     console.log("quiz ended condition met.")
-//     //deduct 10 seconds
-//
